1fead644841af56a2bbdf6e23247050bf2a6394c
DESKTOP-12PLBUK\Lenovo <truonganhkietdev@gmail.com>
2025-10-23 23:49:29 +0700

    Update Auth Service to add role

--- FStreak.Application/Services/Interface/IAuthService.cs
+++ FStreak.Application/Services/Interface/IAuthService.cs
@@ -4,7 +4,7 @@ namespace FStreak.Application.Services.Interface
 {
     public interface IAuthService
     {
-        Task<(bool Succeeded, string Message, AuthResult Result)> RegisterUserAsync(string email, string username, string firstName, string lastName, string password);
+    Task<(bool Succeeded, string Message, AuthResult Result)> RegisterUserAsync(string email, string username, string firstName, string lastName, string password, string role);
         Task<(bool Succeeded, string Message, AuthResult Result)> LoginAsync(string email, string password);
         Task<(bool Succeeded, string Message, AuthResult Result)> RefreshTokenAsync(string accessToken, string refreshToken);
         Task<bool> RevokeTokenAsync(string refreshToken);
